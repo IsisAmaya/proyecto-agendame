be22a23cc26b862a7d15a55fadf97c5073b8fbb6
SMCARDONAV <92750321+SMCARDONAV@users.noreply.github.com>
2023-09-06 20:06:51 -0500

    Cambio models.py

--- Aplicaciones/Freelancer/models.py
+++ Aplicaciones/Freelancer/models.py
@@ -2,13 +2,78 @@ from django.db import models
 
 # Create your models here.
 
+class Country(models.Model):
+    idcountry = models.AutoField(primary_key=True)
+    namecounty= models.CharField(max_length=250)
+ 
+class City(models.Model):
+    idcity = models.AutoField(primary_key=True)
+    namecity= models.CharField(max_length=250)
+    idcountry = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+class Neighborhood(models.Model):
+    idneighborhood = models.AutoField(primary_key=True)
+    nameneighborhood= models.CharField(max_length=250)
+    idcity = models.ForeignKey(City, on_delete=models.CASCADE)
+
+class Services(models.Model):
+    idservices= models.AutoField(primary_key=True)
+    name= models.CharField(max_length=250)
+
+class User(models.Model):
+    iduser= models.AutoField(primary_key=True)
+    typeuser=models.IntegerField()
+    email = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+ 
 class Freelancer(models.Model):
-    codigo = models.CharField(primary_key=True, max_length=6)
-    nameFreelancer=models.CharField(max_length=300)
-    servicios=models.CharField(max_length=250)
-    image =models.ImageField(upload_to='freelancer/images/')
-    ubicacion = models.CharField(max_length=250)
-    disponibilidad = models.CharField(max_length=250)
+    idfreelancer = models.ForeignKey(User, on_delete=models.CASCADE)
+    idcard = models.IntegerField()
+    name=models.CharField(max_length=250)
+    lastname = models.CharField(max_length=250)
+    phone = models.IntegerField()
+    birthday = models.DateField()
+    idcountry = models.ForeignKey(Country, on_delete=models.CASCADE)
+    idcity = models.ForeignKey(City, on_delete=models.CASCADE)
+    idneighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    address = models.CharField(max_length=300)
+    imageprofile =models.ImageField(upload_to='freelancer/images/')
+    imagejobs =models.ImageField(upload_to='freelancer/images/')
+    idservices = models.ForeignKey(Services, on_delete=models.CASCADE)
+
+class Customer(models.Model):
+    idcustomer = models.ForeignKey(User, on_delete=models.CASCADE)
+    idcard = models.IntegerField()
+    name = models.CharField(max_length=250)
+    lastname = models.CharField(max_length=250)
+    phone = models.IntegerField()
+    birthday = models.DateField()
+    imageprofile =models.ImageField(upload_to='customer/images/')
+    idcountry = models.ForeignKey(Country, on_delete=models.CASCADE)
+    idcity = models.ForeignKey(City, on_delete=models.CASCADE)
+    idneighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    address = models.CharField(max_length=300)
+
+class Schedule(models.Model):
+    idschedule= models.AutoField(primary_key=True)
+    title = models.CharField(max_length=250)
+    date = models.DateField()
+    startime = models.TimeField ()
+    endtime = models.TimeField ()
+    state = models.IntegerField()
+
+class Request(models.Model):
+    idrequest = models.AutoField(primary_key=True)
+    idcustomer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    idfreelancer = models.ForeignKey(Freelancer, on_delete=models.CASCADE)
+    idschedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    registerday = models.DateField()
+    state = models.IntegerField()
+
+
+
+
+
     
 
 
--- Aplicaciones/Freelancer/views.py
+++ Aplicaciones/Freelancer/views.py
@@ -13,7 +13,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Freelancer
-
+from .models import Schedule
+from django.http import JsonResponse
 
 
 def home(request):
@@ -24,3 +25,56 @@ def home(request):
     else:
         freelancers = Freelancer.objects.all()
     return render(request, 'home.html', {'searchTerm':searchTerm, 'freelancers' :freelancers})
+
+def calendar(request):
+    all_events = Schedule.objects.all()
+    context = {
+        "events":all_events,
+    }
+    return render(request,'schedule.html',context)
+
+def all_events(request):                                                                                                 
+    all_events = Schedule.objects.all()                                                                                    
+    out = []                                                                                                             
+    for event in all_events:                                                                                             
+        out.append({                                                                                                     
+            'title': event.title,                                                                                         
+            'id': event.id,                                                                                              
+            'start': event.starttme.strftime("%m/%d/%Y, %H:%M:%S"),                                                         
+            'end': event.endtime.strftime("%m/%d/%Y, %H:%M:%S"),                                                             
+        })                                                                                                               
+                                                                                                                     
+    return JsonResponse(out, safe=False)  
+
+def add_event(request):
+    start = request.GET.get("start", None)
+    end = request.GET.get("end", None)
+    title = request.GET.get("title", None)
+    event = Schedule(name=str(title), start=start, end=end)
+    event.save()
+    data = {}
+    return JsonResponse(data)
+
+
+def update(request):
+    start = request.GET.get("start", None)
+    end = request.GET.get("end", None)
+    title = request.GET.get("title", None)
+    id = request.GET.get("id", None)
+    event = Schedule.objects.get(id=id)
+    event.start = start
+    event.end = end
+    event.name = title
+    event.save()
+    data = {}
+    return JsonResponse(data)
+
+
+def remove(request):
+    id = request.GET.get("id", None)
+    event = Schedule.objects.get(id=id)
+    event.delete()
+    data = {}
+    return JsonResponse(data)
+
+
